fa9c7bf9cbee70debade1adf7a2191d935c9db2d
brogram1989 <dilshod.abdusomadogli@gmail.com>
2023-10-20 17:36:16 +0500

    view qismida wordlar chiqazildi

--- fcard/my_card/views.py
+++ fcard/my_card/views.py
@@ -9,9 +9,14 @@ from .models import FlashWord
 #FleshWorddagi barcha wordlarni chiqazib olamiz
 
 def index(request):
-    word = FlashWord.objects.all().values()
-    context = {'word':word, }
+    my_words = FlashWord.objects.all().values()
+
+    #template = loader.get_template('my_card/index.html')
+    #yuqoridagi ikkinchi variant view uchun, lekin natija bir xil bo'ladi
+    context = {'my_words':my_words, }
     return render(request, 'my_card/index.html', context=context)
+    #return HttpResponse(template.render(context, request))
+    # yuqoridagi ikkinchi variant view uchun, lekin natija bir xil bo'ladi
 
 
 
